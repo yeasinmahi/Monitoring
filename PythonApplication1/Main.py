2a8e660ddbe1d85314a3c0f6901e005436b4af72
my.arafat <my.arafat@blmanagedservices,com>
2022-07-05 19:14:34 +0600

    Matplot added

--- PythonApplication1/Main.py
+++ PythonApplication1/Main.py
@@ -1,5 +1,6 @@
 from Oracle_Con import execute
 from Email import Email
+from MatPlot import MatPlot
 
 query = """select trunc(logtime), sum(totalrequest),method from tblapilogsummery
 where applicationname='CFLWebService'
@@ -8,6 +9,7 @@ group by trunc(logtime), method
 order by 1 desc"""
 
 result = execute(query);
-email = Email();
-email.send_mail("test")
+#email = Email();
+#email.send_mail("test")
+MatPlot().Line(result);
 
--- PythonApplication1/MatPlot.py
+++ PythonApplication1/MatPlot.py
@@ -5,6 +5,17 @@ import numpy as np
 class MatPlot(object):
     species = "Diagram"
 
+    def Line(self,data):
+
+        for row in data:
+            print(str(row[0]) + " " + str(row[1]))
+        plt.plot(data[0], data[1])
+        plt.show()
+        #xpoints = np.array([1, 8])
+        #ypoints = np.array([3, 10])
+
+        #plt.plot(xpoints, ypoints)
+        #plt.show()
 
 
 
